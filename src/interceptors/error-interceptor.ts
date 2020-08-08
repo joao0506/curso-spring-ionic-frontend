7ee9bbb5dab58e63b69e9e396074fec37d02c781
joao0506 <joaoholiveira1@hotmail.com>
2020-08-08 15:21:50 -0300

    Salvando novo cliente

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Rx";
 import { StorageService } from "../services/storage.service";
 import { AlertController } from "ionic-angular";
+import { FieldMessage } from "../models/fieldMessage";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
@@ -31,6 +32,9 @@ export class ErrorInterceptor implements HttpInterceptor{
                     this.handle403(); 
                     break;
 
+                case 422:
+                    this.handle422(errorObj);
+                    break;
                 default: 
                 this.handleDefaultError(errorObj);
             }
@@ -58,6 +62,20 @@ export class ErrorInterceptor implements HttpInterceptor{
         alert.present(); 
     }
 
+    handle422(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro 422: Validação',
+            message: this.listErrors(errorObj.errors),
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
+    }
+
     handleDefaultError(errorObj){
         let alert = this.alertCtrl.create({
             title: 'Erro ' + errorObj.status + ': '+errorObj.error,
@@ -70,6 +88,14 @@ export class ErrorInterceptor implements HttpInterceptor{
             ]
         });
         alert.present(); 
+    }
+
+    private listErrors(messages : FieldMessage[]) : string {
+        let s : string = '';
+        for (var i=0; i<messages.length; i++) {
+            s = s + '<p><strong>' + messages[i].fieldName + "</strong>: " + messages[i].message + '</p>';
+        }
+        return s;
     }
 
 }
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -17,5 +17,16 @@ export class ClienteService{
         return this.http.get<ClienteDTO>(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
     }
 
+    insert(obj : ClienteDTO) {
+        return this.http.post(
+            `${API_CONFIG.baseUrl}/clientes`, 
+            obj,
+            { 
+                observe: 'response', 
+                responseType: 'text'
+            }
+        ); 
+    }
+
 
 }
